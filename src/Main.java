fdd45b7e77916a1f654a009a0ad9b6ada976cd0c
antonrogo1 <antonrogo@gmail.com>
2016-02-04 19:29:47 -0600

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World!    ");
-=======
-        System.out.println("Hello World ! ");
->>>>>>> c4520b982a052d9c9ad3a4ba0312f55a04bef3ec
+
+        System.out.println("Hello World! Commit test2");
+
     }
 }
